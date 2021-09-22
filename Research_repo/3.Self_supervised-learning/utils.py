73cb8f4e274248a547d063b52cb03838016b8eb1
geilerloui <geilerloui@gmail.com>
2021-09-22 14:53:54 +0200

    Commit where both files work well. But the clustering is not correct, I improve it in the next commit

--- Research_repo/3.Self_supervised-learning/utils.py
+++ Research_repo/3.Self_supervised-learning/utils.py
@@ -97,19 +97,19 @@ class Net(nn.Module):
         """
         To add dropout or other idk
         """
-        self.fc1 = nn.Linear(in_features =input_size, out_features = 20)
+        self.fc1 = nn.Linear(in_features =input_size, out_features = 10)
         self.fc2 = nn.Linear(in_features = 20, out_features=10)
         self.fc3 = nn.Linear(in_features=10, out_features=output_size)
-        self.bn1 = nn.BatchNorm1d(num_features=20)
+        self.bn1 = nn.BatchNorm1d(num_features=10)
         self.bn2 = nn.BatchNorm1d(num_features=10)
         self.dropout = nn.Dropout(0.25)
+        self.relu = nn.ReLU()
         
     def forward(self, x):
         x = self.fc1(x)
         x = self.bn1(x)
-        #x = F.glu(x)
-        x = self.fc2(x)
-        x = self.bn2(x)
+        #x = self.relu(x)
+
         #x = F.glu(x)
         x = torch.sigmoid(self.fc3(x))
         #x = self.fc2(self.dropout(x))
